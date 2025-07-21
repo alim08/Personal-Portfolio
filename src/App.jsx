f49d0678e42c30cac544db5132b70bd76162e3f1
Adam Lim <alim4@ufl.edu>
2025-07-20 20:04:30 -0400

    updated 3d section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import LogoSection from './sections/LogoSection.jsx'
-import NavBar from './components/NavBar.jsx'
+import Navbar from './components/NavBar.jsx'
 import FeatureCards from './sections/FeatureCards.jsx'
 import Hero from './sections/Hero.jsx'
 import ShowcaseSection from './sections/ShowcaseSection.jsx'
